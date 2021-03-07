f6284db119c3787503910f8c0b82f39126845651
unknown <timothee.monceaux@gmail.com>
2021-03-07 20:28:48 +0100

    Handle connexion to the cloud DB

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => res.send('Hello, World!'));
+
+const client =new Pool();
+client.connect();
+client.query('SELECT * FROM "auth"."Users"', (err, res) => {
+  console.log(err ? err.stack : res) // Hello World!
+  client.end()
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
