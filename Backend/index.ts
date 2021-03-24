68fb2e9e58f976690773ba9a16bb47b7cf83ab13
unknown <timothee.monceaux@gmail.com>
2021-03-24 20:37:48 +0100

    Allow cors for backend

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import authRouter from './routes/auth';
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
