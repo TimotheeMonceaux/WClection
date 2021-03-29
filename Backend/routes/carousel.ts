f5b4a7aa827368c24aef7e54232ffea8417734a4
unknown <timothee.monceaux@gmail.com>
2021-03-29 07:41:49 +0200

    filter out undefined values in carousel slides

--- Backend/routes/carousel.ts
+++ Backend/routes/carousel.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 
-import CarouselSlide from '../models/shop/carouselSlide';
 import { getCarouselSlides } from '../controllers/carousel';
 
 const carouselRouter = express.Router();
@@ -8,7 +7,7 @@ const carouselRouter = express.Router();
 carouselRouter.get('/',
     (req, res) => {
         getCarouselSlides()
-            .then(slides => res.status(200).json({success: true, slides}))
+            .then(slides => res.status(200).json({success: true, slides: slides.map(s => s.toFrontend())}))
             .catch(error => res.status(500).json({success: false, msg: "An unexpected error has occured", error}));
     });
 
