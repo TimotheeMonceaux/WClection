d3f8bcbac012dd9e81d979de2ee399b25b99c166
unknown <timothee.monceaux@gmail.com>
2021-04-15 17:35:25 +0200

    Avoid dual url encoding of email addresses

--- Backend/mail/templates/confirmEmail.ts
+++ Backend/mail/templates/confirmEmail.ts
@@ -3,7 +3,7 @@ import { EmailParameters } from "..";
 const SUBJECT = 'Bienvenue chez WClection ! 😀';
 
 function getValidationUrl(email: string, key: string): string {
-    return `http://wclection.com/confirmEmail?email=${encodeURIComponent(email)}&key=${encodeURIComponent(key)}`;
+    return `http://wclection.com/confirmEmail?email=${email}&key=${key}`;
 }
 
 function getText(email: string, key: string) {
