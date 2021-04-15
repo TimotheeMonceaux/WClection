5e65c3fea9d7d3c66c9f50fb9fda77bdd2fd1ac0
unknown <timothee.monceaux@gmail.com>
2021-04-15 17:31:19 +0200

    Refact massive routes using async await syntax for readability

--- Backend/routes/auth.ts
+++ Backend/routes/auth.ts
@@ -8,82 +8,83 @@ const authRouter = express.Router();
 authRouter.post('/login',
     body('email').isEmail(),
     body('password').isLength({min: 8}),
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({success: false, msg: "Invalid request", errors: errors.array()});
         }
-        
-        retrieveUser(req.body.email)
-            .then(usr => {
-                if (usr === undefined) {
-                    return res.status(401).json({success: false, msg: "Email inconnu"});
-                }
 
-                login(usr!, req.body.password)
-                    .then(st => {
-                        if (!st.success) {
-                            return res.status(401).json({success: false, msg: "Mot de passe incorrect"});
-                        }
-                        return res.status(200).json({success: true, token: st.token, user: usr!.toFrontend()});
-                    })
-            })
-            .catch(e => {console.error(e.stack); return res.status(500).json("An unexpected error has occured.")});
+        try {
+            const usr = await retrieveUser(req.body.email);
+            if (usr === undefined) {
+                return res.status(401).json({success: false, msg: "Email inconnu"});
+            }
+    
+            const st = await login(usr!, req.body.password);
+            if (!st.success) {
+                return res.status(401).json({success: false, msg: "Mot de passe incorrect"});
+            }
+            return res.status(200).json({success: true, token: st.token, user: usr!.toFrontend()});
+        } 
+        catch (e) {
+            console.error(e.stack);
+            return res.status(500).json("An unexpected error has occured.");
+        }
     });
 
 authRouter.post('/signup',
     body('email').isEmail(),
     body('password').isLength({min: 8}),
     body('newsletter').isBoolean(),
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({success: false, msg: "Invalid request", errors: errors.array()});
         }
-        
-        retrieveUser(req.body.email)
-            .then(usr => {
-                if (usr !== undefined) {
-                    return res.status(401).json({success: false, msg: "Cet email est déjà utilisé"});
-                }
-                
-                signup(req.body.email, req.body.password, req.body.newsletter)
-                    .then(st => {
-                        if (!st.success) {
-                            return res.status(500).json({success: false, msg: "Internal server error"});
-                        }
 
-                        return res.status(200).json({success:true, token: st.token, user: new User(req.body.email, '').toFrontend()});
-                    });
-            })
-            .catch(e => {console.error(e.stack); return res.status(500).json("An unexpected error has occured.")});
+        try {
+            const usr = await retrieveUser(req.body.email);
+            if (usr !== undefined) {
+                return res.status(401).json({success: false, msg: "Cet email est déjà utilisé"});
+            }
+
+            const st = await signup(req.body.email, req.body.password, req.body.newsletter);
+            if (!st.success) {
+                return res.status(500).json({success: false, msg: "Internal server error"});
+            }
+            return res.status(200).json({success:true, token: st.token, user: new User(req.body.email, '').toFrontend()});
+        }
+        catch (e) {
+            console.error(e.stack);
+            return res.status(500).json("An unexpected error has occured.");
+        }
     });
 
 authRouter.get('/confirmEmail',
     check('email').isEmail(),
     check('key').isUUID(),
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({success: false, msg: "Invalid request", errors: errors.array()});
+            return res.status(400).json({success: false, msg: "Invalid request.", errors: errors.array()});
         }
 
-        retrieveEmailConfirmationCache(req.query!.email, req.query!.key)
-            .then(ecc => {
-                if (ecc === undefined) {
-                    return res.status(401).json({success: false, msg: "Ce lien n'est plus valide"});
-                }
+        try {
+            const ecc = await retrieveEmailConfirmationCache(req.query!.email, req.query!.key);
+            if (ecc === undefined) {
+                return res.status(401).json({success: false, msg: "Ce lien n'est pas valide."});
+            }
 
-                confirmEmail(ecc.email, ecc.validUntil)
-                    .then(st => {
-                        if (!st.success) {
-                            return res.status(401).json({success: false, msg: "Ce lien n'est plus valide"})
-                        }
-
-                        return res.status(200).json({success:true, token: st.token})
-                    });
-            })
-            .catch(e => {console.error(e.stack); return res.status(500).json("An unexpected error has occured.")});
+            const st = await confirmEmail(ecc.email, ecc.validUntil);
+            if (!st.success) {
+                return res.status(401).json({success: false, msg: "Ce lien n'est plus valide."})
+            }
+            return res.status(200).json({success:true, token: st.token})
+        }
+        catch (e) {
+            console.error(e.stack);
+            return res.status(500).json("An unexpected error has occured.");
+        }
     });
 
 export default authRouter;
