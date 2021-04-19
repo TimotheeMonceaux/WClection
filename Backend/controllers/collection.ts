ba104d3f2931057e784a1645598ac5b198bd5f66
Timothée Monceaux <timothee.monceaux@gmail.com>
2021-04-19 19:30:52 +0200

    implement caching for carousel and products

--- Backend/controllers/collection.ts
+++ Backend/controllers/collection.ts
@@ -3,6 +3,12 @@ import _ from 'lodash';
 import { query, log } from '../db';
 import Collection from '../models/shop/collection';
 import Product from '../models/shop/product';
+import { nowPlusHours } from '../utils/date';
+
+const collectionCache: {
+    revokeDate?: Date,
+    collections?: Array<Collection>
+} = {};
 
 const collectionQuery = `
     SELECT C."Name" AS "Collection", P."Id", P."Name", P."Description", P."BasePrice", P."Price", P."MainImage", P."SecondaryImage", P."Images"
@@ -10,7 +16,19 @@ const collectionQuery = `
     JOIN shop."Collections" C ON P."CollectionId" = C."Id"
     WHERE C."IsActive" AND P."IsActive"`;
 
+function isCacheValid(): boolean {
+    return collectionCache !== {} && new Date() < collectionCache.revokeDate!;
+}
+
+function updateCache(collections: Array<Collection>) {
+    collectionCache.collections = collections;
+    collectionCache.revokeDate = nowPlusHours(12);
+}
+
 export async function getCollections(): Promise<Array<Collection>> {
+    if (isCacheValid())
+        return collectionCache.collections!;
+
     const rows = await query(collectionQuery);
     const grouped = _.groupBy(rows, 'Collection');
     const collections = Object.keys(grouped)
@@ -25,6 +43,8 @@ export async function getCollections(): Promise<Array<Collection>> {
                 i.Description,
                 i.SecondaryImage,
                 i.Images))));
+
+    updateCache(collections);
     log("GET_COLLECTION", undefined, "Success", {collections: collections.length, products: rows.length});
     return collections;
 }
--- Backend/utils/date.ts
+++ Backend/utils/date.ts
@@ -1,3 +1,7 @@
 export function nowPlusMinutes(minutes: number): Date {
     return new Date(Date.now() + minutes * 60000);
 }
+
+export function nowPlusHours(hours: number): Date {
+    return nowPlusMinutes(60 * hours);
+}
