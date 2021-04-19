5e1c004e20dadb53ed71428752697cf77ec52035
Timothée Monceaux <timothee.monceaux@gmail.com>
2021-04-19 23:05:51 +0200

    rename temp

--- Backend/utils/object.ts
+++
@@ -1,4 +0,0 @@
-export function filterNullValues(o: any): any {
-    return Object.keys(o).filter(key => o[key] !== undefined && o[key] !== null)
-                         .reduce((agg, k) => ({...agg, [k]: o[k]}), {});
-}
